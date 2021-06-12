c446e47ef7a8c8cb4e2cbdd037d2b421363202fa
Felix Browarzik <fb@twogroup.de>
2021-06-12 09:43:36 +0200

    BLI1 Task5: Export der Feature Module

--- project-potato/src/app/app.module.ts
+++ project-potato/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { OverviewModule } from './overview/overview.module';
+import { RecipesModule } from './recipes/recipes.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, OverviewModule, RecipesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- project-potato/src/app/recipes/recipes.module.ts
+++ project-potato/src/app/recipes/recipes.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipesComponent } from './recipes.component';
 
-
-
 @NgModule({
-  declarations: [
-    RecipesComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [RecipesComponent],
+  imports: [CommonModule],
+  exports: [RecipesComponent],
 })
-export class RecipesModule { }
+export class RecipesModule {}
