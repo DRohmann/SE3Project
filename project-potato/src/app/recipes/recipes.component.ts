79013f9058c5ca17ce48ed4855d2ca97b089c486
Malte <rhaegariv@gmail.com>
2021-06-12 13:32:15 +0200

    BL1 Task1 Basic Implementation Feature

--- project-potato/src/app/recipes/recipes.component.ts
+++ project-potato/src/app/recipes/recipes.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
 
+// export interface Recipe {
+//   name: string;
+//   preparation: string;
+//   weight: Ingredient[];
+// }
+// export interface Ingredient{
+
+// }
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss']
 })
+
+
+
 export class RecipesComponent implements OnInit {
 
-  constructor() { }
+  recipeGroup: FormGroup;
+  columns: string[];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.columns = ["Name", "Typ", "Dauer"];
+    this.recipeGroup = this.formBuilder.group({
+      tableRowArray: this.formBuilder.array([
+        this.createTableRow()
+      ])
+    })
+  }
+
+  getRecipes():  FormArray {
+    return this.recipeGroup.get('tableRowArray') as FormArray;
+  }
+
+  createTableRow(): FormGroup {
+    return this.formBuilder.group({
+      name: new FormControl(null,{}),
+      type: new FormControl(null,{}),
+      duration: new FormControl(null,{}),
+    })
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
